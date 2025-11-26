17b9a44cf1d1fdb4eaaa445df72acffcbc3d9eaa
Talha Rana <mtalharana093@gmail.com>
2025-11-26 20:15:01 +0500

    database setup

--- app/database/config.py
+++ app/database/config.py
@@ -3,4 +3,15 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-print(BASE_DIR)
+# print(BASE_DIR)
+
+db_path = os.path.join(BASE_DIR, "sqlite.db")
+
+DATABASE_URL = f"sqlite:///{db_path}"
+
+engine = create_engine(DATABASE_URL, echo=True)
+
+
+# Create Table 
+def create_tables():
+  SQLModel.metadata.create_all(engine)
